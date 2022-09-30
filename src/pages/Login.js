908a9a260e5e5eaedf360db15d5e7eeb6b0f86cd
Talita Saez <tltsaez@gmail.com>
2022-09-29 23:58:12 -0300

    resolução parcial requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+import { userLoginAction } from '../redux/actions';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isBtnDisabled: true,
+  };
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.verifyBtn());
+  };
+
+  verifyBtn = () => {
+    const { email, password } = this.state;
+    const regex = /\S+@\S+\.\S+/;
+    const numberPassword = 6;
+    const verifyEmail = regex.test(email);
+    const verifyPassword = password.length >= numberPassword;
+    this.setState({ isBtnDisabled: !(verifyEmail && verifyPassword) });
+  };
+
+  handleBtn = (event) => {
+    event.preventDefault();
+    const { history, userLogin } = this.props;
+    const { email } = this.state;
+    userLogin(email);
+    // dispatch(fetchApi());
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isBtnDisabled } = this.state;
+    return (
+      <div className="login">
+        <h3>Login</h3>
+        <label htmlFor="email">
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            id="email"
+            value={ email }
+            onChange={ this.handleInput }
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            id="password"
+            value={ password }
+            onChange={ this.handleInput }
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={ (event) => this.handleBtn(event) }
+          disabled={ isBtnDisabled }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
+const mapDispatchToProps = (dispatch) => ({
+  userLogin: (payload) => dispatch(userLoginAction(payload)),
+});
 
-export default Login;
+Login.propTypes = {
+  userLogin: propTypes.func.isRequired,
+  history: propTypes.shape.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,4 @@
 // Coloque aqui suas actions
+export const USER_LOGIN = 'USER_LOGIN';
+
+export const userLoginAction = (payload) => ({ type: USER_LOGIN, payload });
